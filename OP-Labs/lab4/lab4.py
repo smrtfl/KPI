474c74ebe23ba29d9d0dad28548ec06819edad48
Pasha Nikulin <p.nikulin13@gmail.ocm>
2021-10-15 19:37:36 +0300

    lab4

--- OP-Labs/lab4/lab4.py
+++ OP-Labs/lab4/lab4.py
@@ -1,27 +1,23 @@
 #Введення початкових значень
-n = float(input("Введіть число n: "))
-a = float(input("Введіть число a: "))
-b = float(input("Введіть число b: "))
+n = float(input("Введіть натуральне число n: "))
+a = float(input("Введіть дійсне число a: "))
+b = float(input("Введіть дійсне число b: "))
 
 i = 1
-h = (b - a)/n
 
 #Якщо n не є натуральним
-while (n * 10) % 10 != 0  or n < 0:
+while (n * 10) % 10 != 0 or n < 0:
     n = float(input("\nn має бути натуральним числом!\nВведіть нове значення n: "))
 
 #Якщо a дорівнює b
 while a == b:
     b = float(input("\na не може дорівнювати b!\nВведіть нове значення b: "))
 
+h = (b - a)/n
+
 #Обчислення членів послідовності
 print("\nПослідовність чисел r: ")
-for i in range(1, int(n)):
+for i in range(i, int(n + 1)):
     ri = a + i * h
     print("r" + str(i) + " = " + str(ri))
     i += 1
-
-#Якщо n дорівнює 1
-if n == 1:
-    ri = a + i * h
-    print("r" + str(i) + " = " + str(ri))
---
+++ OP-Labs/lab4/lab4/lab4/lab4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	/*Ініціалізація змінних*/
+	float a, b, h, ri;
+	int n;
+
+	/*Введення числа n*/
+	cout << "Введіть натуральне число n: ";
+	cin >> n;
+
+	/*Введення числа a*/
+	cout << "Введіть дійсне число a: ";
+	cin >> a;
+
+	/*Введення числа b*/
+	cout << "Введіть дійсне число b: ";
+	cin >> b;
+
+	/*Обчислення числа h*/
+	h = (b - a) / n;
+
+	/*Пошук елементів послідовності R*/
+	cout << "\n" << "Послідовність чисел r:" << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		ri = a + i * h;
+		cout << "r" << i << " = " << ri << endl;
+	}
+
+	return 0;
+}
