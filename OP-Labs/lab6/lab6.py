fc8157c323d55f877f739c99b7a9276f194d9fbd
Pasha Nikulin <p.nikulin13@gmail.ocm>
2021-11-29 14:13:49 +0200

    lab6

--- OP-Labs/lab6/lab6.py
+++ OP-Labs/lab6/lab6.py
@@ -1,22 +1,20 @@
 import math
 
-#eps = float(input("Введіть точність обчислення: "))
-#a = float(input("Введіть дійсне a : "))
-#b = float(input("Введіть дійсне b: "))
+a = float(input("Введіть дійсне a: "))
+b = float(input("Введіть дійсне b: "))
+eps = float(input("Введіть точність обчислення: "))
 
 n = 0
 
-def cos(x):
+#Функція обчислення ряду Тейлора
+def cos(x1, n1, eps1):
 
-    sum = (((-1) ** n) * (x ** (2 * n))) / math.factorial(2 * n)
-    n += 1
+    sum = (((-1) ** n1) * (x1 ** (2 * n1))) / math.factorial(2 * n1)
 
-    if math.fabs(sum) < eps:
+    if math.fabs(sum) < eps1:
         return 0
+    else:
+        return sum + cos(x1, n1 + 1, eps1)
 
-    return cos(x)
-
-print(cos(4))
-
-#y = cos(a) + (cos(b) ** 2) + cos (a + b)
-#print("cos(a) + (cos(b) ** 2) + cos (a + b) = " + y)
+y =  cos(a, n, eps) + (cos(b, n, eps) ** 2) + cos (a + b, n, eps)
+print("\ncos({a}) + cos({b})^2 + cos ({c}) = {y}".format(a = a, b = b, c = a + b, y = y)
